2c2c4a8daee918e40fb26bf2d67486602905f8cd
Juliana Katz <julianalimacunha@hotmail.com>
2023-09-18 20:17:33 -0400

    Update account id

--- bin/awsec3_express_docker.ts
+++ bin/awsec3_express_docker.ts
@@ -4,7 +4,7 @@ import cdk = require('@aws-cdk/core');
 import { Awsec3ExpressDockerStack } from '../lib/awsec3_express_docker-stack';
 
 const app = new cdk.App();
-const envUSA = { account: '048416854569', region: 'us-east-1' };
+const envUSA = { account: '347694139041', region: 'us-east-1' };
 
 new Awsec3ExpressDockerStack(app, 'Awsec3ExpressDockerStack',{env: envUSA} );
 app.synth();
